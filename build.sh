bdc7e6d16dcc0a7ad6b75c4ec48cb8c1f596018c
Jochen Roth <j.roth@zeroseven.de>
2019-05-02 16:30:05 +0200

    Add toml

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --pull . -f Dockerfile -t zeroseven/toml-checker:1.0 && docker run -it -v `pwd`/tests.sh:/tmp/test.sh zeroseven/toml--checker:1.0 /tmp/test.sh
+docker build --pull . -f Dockerfile -t zeroseven07/toml-checker:latest
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# some commands must be executable
-rsync --version || exit 1
-node --version     || exit 1
-surf --version     || exit 1
-toml-checker --version || exit 1
